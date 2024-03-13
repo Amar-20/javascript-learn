1c0d0248d2f13b419d603e2a26e03b1aaf5f672b
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-03-13 22:02:00 +0000

    code

--- js_ba/01_variables.js
+++ js_ba/01_variables.js
@@ -1,1 +1,18 @@
 // Variables and constants
+// note :----Varibales are case sencitive.
+const  accountId=1432;          //////Here 'const' is the keyword,'accountId' is the variable name and it does't change if we assign a new value in future.
+let accountEmail= 'mail@1234';
+var accountNumber='54321';     //Perfer not to use var while assigning variable names, becoz of issue in block scope and functional scope
+accountCity='hyderabad';      // javascript is fergiving but, don't use this type of variable naming.
+
+
+let accountCountry                // Here we can just name the variable without giving the variable value to it.
+
+// These are the sample results if we chnage the variable values
+// accountId=432                // not allowed
+// accountEmail='234@mail'     // This is allowed and value is also chnaged 
+// accountNumber='09876'      //This is allowed but, it's not advisable to use.
+// accountCity='bangalore'   // This  is allowed but , it's the best practice to use this way to either to name a variable and change a varibale name.
+ 
+console.log(accountCity);
+console.table([accountId,accountEmail,accountNumber,accountCity,accountCountry]) // We can print multiple number of outputs in a table format.
---
+++ js_ba/datatypes.js
@@ -0,0 +1,19 @@
+            // There are two types of datatypes 
+       // 1) Primitive datatypes or Value Types
+"use strict"        // just to indecate that we are using the newer version of js
+let name='Amar'    //String (Here Amar is the string ) 
+let number=420    // Number  
+let Yesorno=true //Boolean (just True or false)
+let country;   //Unidentified (unidentified and here only varibale name is defined)
+let binary=0  //Null 
+// Symbol 
+
+
+
+console.table([name,number,Yesorno,country,binary])
+console.log(3+3)
+
+
+// 2) reference datatypes
+// *objects
+// *unidentified
