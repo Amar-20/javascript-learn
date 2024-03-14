e0ab9c30e668bed83e2b13d1e1956859c56c12d2
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-03-14 18:11:23 +0000

    code

---
+++ js_ba/04_comparison.js
@@ -0,0 +1,21 @@
+console.log(null>0)
+console.log(null>=0)  /* output is "True" because in javascript comparisons(<,>,>=,<=) work differently.*/
+console.log(null==0)  // Output is "False" because in javascript "Equality Check"(==) work differently.
+
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+
+
+/*************************************************************************** Basic comparisons *************************************/
+
+console.log(2>3)
+console.log(23=22)
+console.log(32<72)
+console.log(23>=23)
+
+
+// '===' (strict check)  here both values and datatypes are comppared. example
+console.log('2'=== 2)
