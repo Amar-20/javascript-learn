bf971f0ba43be829509f5ef48d8f114159522046
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-03-16 00:55:35 +0000

    code

---
+++ 02_jsbasics/02_arrays.js
@@ -0,0 +1,41 @@
+const myArr=["Thor",'ironman','spiderman']
+const myArry=['Superman','flash','batman']
+
+console.log(myArr)
+console.log(myArry)
+
+// const pusharry= myArr.push(myArry)
+// console.log(pusharry)
+
+
+const newarry=myArr.concat(myArry)       /////concat joins two arrays 
+console.log(newarry)
+
+
+////////////////////////////////////////////we can also use spread insted of concat.Becoz it will help expansion of elements from an iterable (like an array, object, or string) into another iterable or function call.
+
+
+const heros=[...myArr,...myArry]    /// spread operater
+console.log(heros)
+
+
+const newarr=[1,2,4,[5,6,7,],8,[9,10,[11,12]]]
+const realarr=newarr.flat(1)       ///// Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth. And we have to give number of depth it has to concat.
+console.log(realarr)
+
+
+
+const name= "Amar"          
+console.log(Array.isArray(name))       //// This is to check a give element is array or not
+
+//////////////////////////////////////// now we convert it to array.
+
+const arrayName= "Amar"
+console.log(Array.from(arrayName))    /// Now the name is converted it array.
+
+
+
+const A=100
+const B=200
+const C=300
+console.log(Array.of(A,B,C))       // We can also convert variable values into arrays.
