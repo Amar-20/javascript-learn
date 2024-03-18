daeda806e71e21a35bf34d3efc4d78ddf639a8ea
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-03-18 18:06:11 +0000

    code

---
+++ 02_jsbasics/05_objects.js
@@ -0,0 +1,20 @@
+  /* De-Structuring of objects */
+
+  const course={
+    coursename:'Web dev',
+    cost:1000,
+    coursementor:'hitesh'
+
+  } 
+
+// const { coursementor} =course         /* This is called de_strructuing the object */
+// console.log(coursementor)
+
+
+const {coursementor:mentor}=course
+console.log(mentor)                     /* This is also  called as de-structuing of object */
+
+
+
+
+/*  */
