4ac3852d9e1faf5bc0da56504358b4c5136d03d0
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-03-21 16:24:47 +0000

    code

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month=3
+switch (month) {                        // We can use Switch to compare one value with number if different values.
+    case 1:
+        console.log('January')
+        break;
+    case 2:
+        console.log('Feb')
+        break;
+    case 3:
+        console.log('March')
+        break;
+    case 4:
+        console.log('April')
+        break;
+
+    default:
+        console.log('default case match')
+        break;
+}
+
---
+++ 04_control_flow/truthyNfalsy.js
@@ -0,0 +1,48 @@
+const userEmail=[]
+if (userEmail){
+    console.log('got the Email')
+
+}
+else{
+    console.log("Don't have user Email")
+}
+
+//falsy values
+// false, 0,-0,Bigint 0n,"",null,undefined,NaN
+
+//Truthy values
+// true,"false" ,"0"," ",[],{},function(){}
+
+/**************************************************************************************  */
+
+console.log(userEmail.length)                
+if (userEmail.length===0){                     // checking the array is empty are not
+    console.log("Array is empty")
+}                             
+
+/******************************************************************************************************* */
+  
+
+const empobj={}
+if(Object.keys(empobj).length===0){
+    console.log("object is empty")
+}
+
+/**************************************************** Nullish coalescing Operator(??) ********************************* */
+
+let val1
+val1=40??50
+val1=null??30
+val1=undefined??10
+val1=null??5??10
+
+
+console.log(val1)
+
+
+/********************************************************** Terniary operator(?)  ************************** */
+
+// condition?true:false                  // syntax
+
+const iceCreamPrice=5.00
+iceCreamPrice <= 4.00? console.log('Less than 4.00') ``: console.log('more than 4.00')
