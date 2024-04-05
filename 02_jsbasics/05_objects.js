11871ae402c835a92de54457a6fd9fb0b8e34ccb
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-04-05 15:49:34 -0400

    Update 05_objects.js

--- 02_jsbasics/05_objects.js
+++ 02_jsbasics/05_objects.js
@@ -12,10 +12,81 @@
 
 
 const {coursementor:mentor}=course
-console.log(mentor)                     /* This is also  called as de-structuing of object */
+console.log(mentor)                     /* This is also  called as de-structuring of object */
 
 
 
+
+/*--------------------------------------------------------------- Array de-Structuring------------------------------------------- */
+ ```const colors = ['red', 'green', 'blue'];
+ const [firstColor, secondcolor, thirdColor] = colors;
+
+console.log(firstColor);  // Output: red
+console.log(secondColor); // Output: green
+console.log(thirdColor);  // Output: blue```
+
+/*-----------------------------------------------------------------------------------------------------------------------------*/
+```const numbers = [1, 2, 3, 4, 5];
+
+const [firstNumber, , thirdNumber] = numbers;
+
+console.log(firstNumber);  // Output: 1
+console.log(thirdNumber);  // Output: 3```
+
+
+/*-------------------------- You can use the rest parameter syntax (...) to collect remaining array elements into a separate array:------------------------*/
+```const numbers = [1, 2, 3, 4, 5];
+
+const [firstNumber, ...remainingNumbers] = numbers;
+
+console.log(firstNumber);       // Output: 1
+console.log(remainingNumbers);  // Output: [2, 3, 4, 5]
+```
+
+
+/*--------------------------------------------------------------------Object de-Structuring ---------------------------------------------*/
+
+```const person = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+
+const { name, age, city } = person;
+
+console.log(name); // Output: John
+console.log(age);  // Output: 30
+console.log(city); // Output: New York
+```
+/*-----------------------You can also assign extracted properties to variables with different names:----------------------*/
+
+```const person = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+
+const { name: personName, age: personAge, city: personCity } = person;
+
+console.log(personName); // Output: John
+console.log(personAge);  // Output: 30
+console.log(personCity); // Output: New York
+```
+/*--------------------------You can use the rest pattern (...) to capture remaining properties into a separate object:--------------------------*/
+
+```const person = {
+  name: 'John',
+  age: 30,
+  city: 'New York',
+  country: 'USA'
+};
+
+const { name, age, ...rest } = person;
+
+console.log(name); // Output: John
+console.log(age);  // Output: 30
+console.log(rest); // Output: { city: 'New York', country: 'USA' }
+```
 
 /* JSON API intro */
 
