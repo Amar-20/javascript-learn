22cfdc1f363156d62a196ecd86e5e1d43e956b9c
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-04-08 10:00:29 -0400

    Update 01_functions.js

--- 03_jsbasics/01_functions.js
+++ 03_jsbasics/01_functions.js
@@ -1,5 +1,26 @@
  /* Functions */
 
+
+
+// function add(num1,num2){
+//     console.log(num1+num2)
+// }
+// const result=add(12,4)
+// console.log(result)
+
+
+function add(num1,num2){
+    let result =num1+num2
+    return result
+
+}
+const result=add(12,34)
+console.log("Result:-",result)
+
+
+
+
+
 //  function sayMyName(firstn,lastn){
 
 //     console.log(firstn + lastn)                       /*   function 1    */
