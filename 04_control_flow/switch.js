4bff18cf0acdfee34fc71ac3ffd3cad11a44ea8a
Amar-20 <145368973+Amar-20@users.noreply.github.com>
2024-04-10 11:27:55 -0400

    Update switch.js

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -18,3 +18,28 @@ switch (month) {                        // We can use Switch to compare one valu
         break;
 }
 
+/*    With using function                */
+
+
+function knowMonth(num=4){        //num=4 default value
+switch (num) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("Mar")
+        break;
+    case 4:
+        console.log("Apr")
+        break;
+
+
+    default:
+        console.log("none of the above")
+        break;
+}
+}
+knowMonth(5)                         // output is none of the above.
